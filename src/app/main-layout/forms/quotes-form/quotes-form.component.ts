b8281044000ead9425aba94494b0ed0df1b1cdbb
Haim Fridland <hfridland@shaw.ca>
2021-11-20 18:49:46 -0800

    Report

--- src/app/main-layout/forms/quotes-form/quotes-form.component.ts
+++ src/app/main-layout/forms/quotes-form/quotes-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from 'src/app/services/report.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-quotes-form',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes-form.component.scss']
 })
 export class QuotesFormComponent implements OnInit {
+  repToken: string = '';
 
-  constructor() { }
+
+  constructor(private reportService: ReportService) { }
 
   ngOnInit(): void {
+  }
+
+  onGetReportClick() {
+    this.reportService.getQuoteWithoutSDPriceReport('652')
+    /*.subscribe(token => {
+      console.log(token);
+      //this.repToken = token;
+    })*/
+  }
+
+  getRepUri() {
+    const ret = `${environment.srvHost}api/report/QuoteWithoutSDPrice/${this.repToken}`;
+    console.log(ret)
+    return ret;
   }
 
 }
---
+++ src/app/services/report.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class ReportService {
+  public error$: Subject<string> = new Subject<string>();
+
+  constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse) {
+    const { message } = error;
+    this.error$.next(message);
+
+    //console.log(error);
+    return throwError(error);
+  }
+
+  getQuoteWithoutSDPriceReport(quoteId: string) {
+    this.http.get(`${environment.srvHost}api/reportToken/QuoteWithoutSDPrice/${quoteId}`, {
+      responseType: 'text'
+    })
+      .pipe(
+        catchError(this.handleError.bind(this))
+      )
+      .subscribe(token => {
+        // console.log('token', token);
+        window.open(`${environment.srvHost}api/report/QuoteWithoutSDPrice/${token}`, '_blank');
+      });
+  }
+
+}
